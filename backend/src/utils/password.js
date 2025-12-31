3c756fa59c4ce86b8adc19016fafeef19425cb86
MALAKKL <150433395+MALAKKL@users.noreply.github.com>
2025-12-31 01:35:52 +0100

    fixed the middleware andsome other utils

---
+++ backend/src/utils/password.js
@@ -0,0 +1,16 @@
+// password.js
+const bcrypt = require("bcrypt");
+
+const hashPassword = async (password) => {
+  const salt = await bcrypt.genSalt(10);
+  return bcrypt.hash(password, salt);
+};
+
+const comparePassword = async (password, hashedPassword) => {
+  return bcrypt.compare(password, hashedPassword);
+};
+
+module.exports = {
+  hashPassword,
+  comparePassword,
+};
